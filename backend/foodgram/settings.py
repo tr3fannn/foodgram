6ee9ff92b4cbecb32436f2cdb573915fe92196f0
Ярослав Богданов <givik7788@gmail.com>
2024-11-07 02:50:34 +0300

    finaL

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -17,12 +17,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-c^n%tnj2dsgxwq#_5!h#^_l=%2*e@ql*5opqs58@_w3c@pcika'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -35,8 +29,6 @@ ALLOWED_HOSTS = [
     '51.250.22.11',
     'tr3fannnn.zapto.org',
 ]
-
-
 
 # Application definition
 
@@ -87,21 +79,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'postgres',
-        #'USER': 'postgres',
-        # 'PASSWORD': 'postgres',
-        # 'HOST': 'db',
-        # 'PORT': '5432',
-    # }
-# }
 
 DATABASES = {
     'default': {
